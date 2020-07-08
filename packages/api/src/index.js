6dc08f85b3801478c02326f6eb177a86744eadcd
Logqwerty <inm2790@gmail.com>
2020-07-09 01:31:50 +0900

    evn test

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,17 +1,36 @@
+require("dotenv").config();
 const express = require("express");
+const monogoose = require("mongoose");
 const { ApolloServer, gql } = require("apollo-server-express");
+
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+const UserSchema = new monogoose.Schema({
+  name: { type: String, require: true },
+  age: { type: Number, require: true },
+});
+const UserModel = monogoose.model("User", UserSchema, "users");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    user: User
+  }
+
+  type User {
+    name: String
+    age: Int
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    user: () => UserModel.findOne({ name: "aaa" }),
   },
 };
 
@@ -20,4 +39,21 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => console.log(`🚀 Server ready!`));
+(async () => {
+  try {
+    await monogoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+      auth: {
+        user: DB_USER,
+        password: DB_PASSWORD,
+      },
+      authMechanism: "SCRAM-SHA-1",
+      authSource: "admin",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.info("db connection success!");
+    app.listen({ port: 4000 }, () => console.log(`🚀 Server ready!`));
+  } catch (err) {
+    console.error(err);
+  }
+})();
